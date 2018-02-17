5b82f535351d44757fbbbfd8ae355d0e9039a958
Erryl Alvares <erryl1@hotmail.com>
2018-02-17 15:00:54 +0530

    Package attribute tables

---
+++ PetaDB/dbo/Tables/PackageValidity.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PackageValidity]
+(
+	[PVId] INT NOT NULL PRIMARY KEY, 
+    [PackageId] INT NOT NULL, 
+    [ValidFrom] DATE NOT NULL, 
+    [ValidTo] DATE NOT NULL,
+	CONSTRAINT [FK_PackageValidity_ToPackage] FOREIGN KEY ([PackageId]) REFERENCES [Package]([PackageId])
+)
