592e6cc0c68820d4ebd9d5cc89fe584d0eae5e65
diptesh03 <diptesh03@gmail.com>
2018-04-04 14:57:59 +0530

    Mice page

---
+++ PetaStarter/Controllers/MiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Speedbird.Controllers
+{
+    public class MiceController : EAController
+    {
+        public ActionResult Index(int? id)
+        {
+
+            return View(base.BaseCreateEdit<MiceDetail>(id, "MiceID"));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "MiceID,GuestName,TDate,Phone,Email,AgentName,Detail")] MiceDetail item)
+        {
+            item.TDate = DateTime.Now;
+            return base.BaseSave<MiceDetail>(item, item.MiceID > 0);
+        }
+    }
+}
