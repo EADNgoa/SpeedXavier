071cb4ad9982eb7b4b2ee6f788599fe4aba3094d
diptesh03 <diptesh03@gmail.com>
2018-05-11 16:23:45 +0530

    Post deployment script

--- PetaDB/dbo/PostDeployment.sql
+++ PetaDB/dbo/PostDeployment.sql
@@ -4,6 +4,12 @@ BEGIN
 	SET IDENTITY_INSERT [dbo].[UserFunctions] ON
 	INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (1, N'User Rights', N'UserRights')
 	INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (1002, N'Holiday', N'Masters')
+    INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (1003, N'Leave Type', N'Masters')
+    INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (1004, N'Leave Entitlement', N'Masters')
+	    INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (1005, N'LeaveApprove', N'Masters')
+		    INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (1006, N'Leave Application', N'Leave Application')
+
+
 
 
 	SET IDENTITY_INSERT [dbo].[UserFunctions] OFF
