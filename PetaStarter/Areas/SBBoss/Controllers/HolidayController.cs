8331ddce3291e56caa95f67df48dfd39215e7be2
diptesh03 <diptesh03@gmail.com>
2018-05-11 14:43:37 +0530

    Manage Holidays

---
+++ PetaStarter/Areas/SBBoss/Controllers/HolidayController.cs
@@ -0,0 +1,53 @@
+﻿using Speedbird.Controllers;
+using System;
+//using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+//using System.Web;
+using System.Web.Mvc;
+
+
+namespace Speedbird.Areas.SBBoss.Controllers
+{
+    public class HolidayController : EAController
+    {
+        // GET: Clients
+        [EAAuthorize(FunctionName = "Holiday", Writable = false)]
+        public ActionResult Index(int? page ,string AN )
+        {
+            if (AN?.Length > 0) page = 1;
+            return View("Index", base.BaseIndex<Holiday>(page, " * ","Holidays Where HolidayName like '%" + AN + "%'"));
+        }
+
+
+
+        // GET: Clients/Create
+        public ActionResult Manage(int? id)
+        {
+           // ViewBag.UnitID = new SelectList(db.Fetch<Unit>("Select UnitID,UnitName from Units"), "UnitID", "UnitName");
+            
+            return View(base.BaseCreateEdit<Holiday>(id, "HolidayID"));
+        }
+
+        // POST: Clients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Manage([Bind(Include = "HolidayID,HDate,HolidayName")] Holiday item)
+        {
+            return base.BaseSave<Holiday>(item, item.HolidayID > 0);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
