847860ef903feb2782ec3c46a3810daec2c54333
Erryl Alvares <erryl1@hotmail.com>
2018-07-30 18:46:13 +0530

    Fixed User Rights screens and SRdetails screens

--- PetaDB/dbo/PostDeployment.sql
+++ PetaDB/dbo/PostDeployment.sql
@@ -37,8 +37,9 @@ BEGIN
     INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (32, N'Car & Bike', N'Car & Bike')
     INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (33, N'Tax', N'Tax')
     INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (34, N'Questions', N'Questions')
-	    INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (35, N'Reciept', N'Reciept')
-		    INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (36, N'Group', N'Group')
+	INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (35, N'Reciept', N'Reciept')
+	INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (36, N'Group', N'Group')
+	INSERT INTO [dbo].[UserFunctions] ([FunctionID], [FunctionName], [Module]) VALUES (36, N'Supplier', N'Supplier')
 
 
 
