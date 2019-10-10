0e1df5d71016c41363536105cde1f0a196059799
Erryl Alvares <erryl1@hotmail.com>
2019-10-10 21:38:24 +0530

    Backward compatible string_split

---
+++ PetaDB/dbo/Functions/STRING_SPLIT.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION dbo.STRING_SPLIT
+    (
+       @List       NVARCHAR(50),
+       @Delimiter  NVARCHAR(3)
+    )
+    RETURNS TABLE
+    WITH SCHEMABINDING
+    AS
+       RETURN 
+       (  
+          SELECT [value] = y.i.value('(./text())[1]', 'nvarchar(5)')
+          FROM 
+          ( 
+            SELECT x = CONVERT(XML, '<i>' 
+              + REPLACE(@List, @Delimiter, '</i><i>') 
+              + '</i>').query('.')
+          ) AS a CROSS APPLY x.nodes('i') AS y(i)
+       );
+    GO
